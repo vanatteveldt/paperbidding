a2191bf6ad780235826fa68a0afeff6c09b2798f
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-10-25 11:46:12 +0200

    Improve assign.py

--- assign.py
+++ assign.py
@@ -8,6 +8,9 @@ import sys
 
 from collections import defaultdict
 
+import django; django.setup()
+from bidding.models import Paper, Author
+
 c, reviewers, papers, solution = defaultdict(set), defaultdict(set), defaultdict(set), defaultdict(set)
 
 for row in csv.DictReader(sys.stdin):
@@ -25,21 +28,28 @@ def argmin(x, func):
             result, value = e, v
     return result
 
+def weight(r):
+    return len(reviewers[r]) + (100 if len(solution[r]) == 2 else 0)
+
 while papers:
     p = argmin(papers, lambda p: len(papers[p]))
     candidates = papers[p] - {r for (r,p) in solution.items() if len(p) >= 3}
     for r in [1,2,3]:
         if not candidates:
-            print("NO CANDIDATES FOR ", p)
+            raise Exception("NO CANDIDATES FOR {p}".format(**locals()))
             break
-        r = argmin(candidates, lambda r: len(reviewers[r]))
+        r = argmin(candidates, weight)
         candidates.remove(r)
         reviewers[r].remove(p)
         solution[r].add(p)
     del papers[p]
 
 w = csv.writer(sys.stdout)
-#w.writerow(["paper", "reviewer"])
+w.writerow(["email", "first_name", "last_name", "paper", "title", "reviewer"])
 
-for r, p in solution.items():
-    print(len(p), r, p, c[r], all(x in c[r] for x in p))
+for r, papers in solution.items():
+    a = Author.objects.get(email=r)
+    for p in papers:
+        p = Paper.objects.get(pk=p)
+        w.writerow([a.email, a.last_name, a.first_name, p.id, p.title,r])
+    #print(len(p), r, p, c[r], all(x in c[r] for x in p))
